540bdcb9076eb1efe2eb63530bda22b9474f2ab7
Aarya Patil <122004182+Aarya2892@users.noreply.github.com>
2025-06-19 07:28:43 +0000

    JavaScript basic and Data types

---
+++ 01_Basics.js
@@ -0,0 +1,13 @@
+console.log("hello Aarya")
+
+const Id = 1
+let name = "Aarya"
+var password = "Aarya1234" //prefer to do not used the var 
+email = "one@gmail.com" //bad practice
+
+console.log(Id)
+
+// Id = 2 we cannot change the const variable value 
+// console.log(Id)
+
+console.table([Id, name, password, email])
---
+++ 02_DataType.js
@@ -0,0 +1,29 @@
+//In javaScript we have primitive and non-primitive data type
+
+//primitive 
+
+/*
+number
+bigInt
+string
+boolean
+symbol
+undefined
+null
+*/
+
+const Id = 1
+const name = "Aarya"
+let isloggedIn = false
+let password; //undefined
+let authorized = null 
+
+console.table([Id, name, isloggedIn, password, authorized])
+console.log(typeof password); //undefined
+console.log(typeof authorized); //object
+
+//non=primitive data type
+/*
+object
+*/
+
