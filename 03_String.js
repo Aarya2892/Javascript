dde5a8f6dffd32df52f103c3f8eb0a711c505024
Aarya Patil <122004182+Aarya2892@users.noreply.github.com>
2025-06-25 16:16:41 +0000

    String

--- 03_String.js
+++ 03_String.js
@@ -8,6 +8,8 @@ console.log(`Hello everyone my name is ${name} and I'm ${age} years old`)
 
 // There are lots of methos we have in string
 /*
+charAt
+indexof
 toLowercase
 toUppercase
 split()
@@ -16,6 +18,13 @@ slice
 replace
 */
 
+const gameName = "Freefire"
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('i'));
+console.log(gameName.toLowerCase());
+console.log(gameName.toUpperCase());
+
+
 // trim remove the white spaces 
 const str= "     hello Aarya     " 
 console.log(str.trim());
@@ -23,3 +32,11 @@ console.log(str.trim());
 // split method split the words into array 
 const str1 = "hello world"
 console.log(str1.split(" ")); //['hello', 'world']
+
+//replace("replace world", "new world")
+console.log(str1.replace("hello", "Hi"))
+
+//slice(start, end)
+const str2 = "myname"
+console.log(str2.slice(3)); //ame
+
