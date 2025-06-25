fb68ef18bedb0d8b980e34f0c47d06ccc475826f
Aarya Patil <122004182+Aarya2892@users.noreply.github.com>
2025-06-25 16:06:47 +0000

    String

---
+++ 03_String.js
@@ -0,0 +1,25 @@
+// String is the most imp part of coding
+
+const name = "Aarya"
+const age = 21
+
+// to concatination please used modern way 
+console.log(`Hello everyone my name is ${name} and I'm ${age} years old`)
+
+// There are lots of methos we have in string
+/*
+toLowercase
+toUppercase
+split()
+trim()
+slice
+replace
+*/
+
+// trim remove the white spaces 
+const str= "     hello Aarya     " 
+console.log(str.trim());
+
+// split method split the words into array 
+const str1 = "hello world"
+console.log(str1.split(" ")); //['hello', 'world']
