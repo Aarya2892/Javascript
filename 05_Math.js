1aeadaa45ada22eae551997e59117ff1f0dec4f7
Aarya Patil <122004182+Aarya2892@users.noreply.github.com>
2025-07-14 16:16:35 +0000

    Math

---
+++ 05_Math.js
@@ -0,0 +1,16 @@
+console.log(Math); //object
+
+console.log(Math.abs(-4)) //to change negative to positive
+
+console.log(Math.round(4.6)) //5 round
+console.log(Math.ceil(4.2))  //5 big 
+console.log(Math.floor(4.2)) //4 lower
+
+console.log(Math.random()) // It  will create random value between 0 to 1
+
+// Math random generate value between 0 to 1 eg. 0.07
+// *10 for one decimal increament 0.7 
+// +1 we want value greater than 1, like this eg if we get value 0 then we will add it with +1 then we will get value between 1 to 10
+// Math floor for the single digit value eg. 7
+console.log(Math.floor(Math.random()*10 +1))
+
