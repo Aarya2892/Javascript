a00996294060cea9ed0e987ed66c0b5076220e6b
Aarya Patil <122004182+Aarya2892@users.noreply.github.com>
2025-07-14 16:27:27 +0000

    Date and TIme

---
+++ 06_DateANDTime.js
@@ -0,0 +1,22 @@
+let myDate = new Date(); //like this we can set the new date
+
+console.log(myDate)
+console.log(typeof myDate) //object
+
+//to make the date in readable format
+console.log(myDate.toString())           //Mon jul 14 2025 16:21:26 GMT+0000
+console.log(myDate.toLocaleDateString()) //7/14/2025
+console.log(myDate.toDateString())       //Mon jul 14 2025
+
+
+// you can customize your own date
+let newDate = new Date(2023, 0, 23)
+console.log(newDate);
+
+// If you want to compare two time stamp that time you neeed .getTime() method
+
+let mytime = Date.now();
+console.log(mytime);
+console.log(newDate.getTime());
+
+console.log(Math.floor(Date.now()/1000));
