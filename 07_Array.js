b362aa92b370ba534ae59773af1dbeb08a40fb27
Aarya Patil <122004182+Aarya2892@users.noreply.github.com>
2025-07-14 16:40:55 +0000

    Array

---
+++ 07_Array.js
@@ -0,0 +1,23 @@
+const arr = [1,2,3,4,5,6]
+
+// arr.push(7); //pop
+// console.log(arr);
+
+// arr.pop() //It will remove last element
+// console.log(arr)
+
+// console.log(arr.includes(4));
+// console.log(arr.indexOf(4))
+
+// arr.unshift(9)
+// console.log(arr)
+
+
+// Slice and splice is most imp in arr
+
+const arr1 = [1,2,3,4,5,6,7]
+console.log(arr1)
+console.log(arr1.slice(1,4)) //[2,3] It will start from index 1 and take upto 3 it will not take 4
+
+console.log(arr1.splice(1,4)); //It will create two arr one with mention index and one for remaining one and return first one
+console.log(arr1) //[1,6,7]
