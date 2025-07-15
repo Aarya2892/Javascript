8f04f35834fd16b522392f08ac0516eff0e276e6
Aarya Patil <122004182+Aarya2892@users.noreply.github.com>
2025-07-15 15:57:09 +0000

    Array

--- 07_Array.js
+++ 07_Array.js
@@ -26,5 +26,28 @@ const marvel_heros = ["thor", "Ironman", "spiderman"]
 const Dc_heros = ["superman", "flash", "batman"]
 
 // to merge the two array
-console.log(marvel_heros.push(dc_heros))
+// marvel_heros.push(Dc_heros)
+// console.log(marvel_heros)
 
+marvel_heros.concat(Dc_heros)
+console.log(marvel_heros);
+
+// another way to merge array (spread operation)
+const all_new_heros = [...marvel_heros, ...Dc_heros]
+console.log(all_new_heros);
+
+
+// flat returns new array with all sub array
+const flat_array = [1,2, [3,4], [5,[6,7]], 8]
+const real_array = flat_array.flat(Infinity)
+console.log(real_array);
+
+
+// sometimes we fetch the data from UI and it is in different different format but we want that data in array format then used Array.from
+console.log(Array.from("Aarya")) //it will convert it into array
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2,score3))
