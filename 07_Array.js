8a7b3b85ad985d1df4cd9eb017d5ea9b6a0dbd4b
Aarya Patil <122004182+Aarya2892@users.noreply.github.com>
2025-07-15 21:17:14 +0530

    Update 07_Array.js

--- 07_Array.js
+++ 07_Array.js
@@ -21,3 +21,10 @@ console.log(arr1.slice(1,4)) //[2,3] It will start from index 1 and take upto 3 
 
 console.log(arr1.splice(1,4)); //It will create two arr one with mention index and one for remaining one and return first one
 console.log(arr1) //[1,6,7]
+
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const Dc_heros = ["superman", "flash", "batman"]
+
+// to merge the two array
+console.log(marvel_heros.push(dc_heros))
+
