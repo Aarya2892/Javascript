0c5d476a36339b22d7754ef31430e4be16a49b54
Aarya Patil <122004182+Aarya2892@users.noreply.github.com>
2025-07-21 06:57:59 +0000

    Object

--- 08_object.js
+++ 08_object.js
@@ -1,24 +1,24 @@
 // If you want to add symbol in object the it will written as [mySym]
-const mySym = Symbol("key1")
+// const mySym = Symbol("key1")
 
-const jsUser ={
-    name : "Aarya",
-    [mySym] : "symbol",
-    age : 22,
-    email : "one@gmail.com",
-    isLoggedIn : false,
-    lastLoggedIn : ["Monday", "saturday"],
-    "full name" : "Aarya Patil"
-}
+// const jsUser ={
+//     name : "Aarya",
+//     [mySym] : "symbol",
+//     age : 22,
+//     email : "one@gmail.com",
+//     isLoggedIn : false,
+//     lastLoggedIn : ["Monday", "saturday"],
+//     "full name" : "Aarya Patil"
+// }
 
-console.log(jsUser)
+// console.log(jsUser)
 
 // sometimes values are get written into string to fetch that used
-console.log(jsUser["full name"])
+// console.log(jsUser["full name"])
 
-jsUser.email = "two@gamail.com"
+// jsUser.email = "two@gamail.com"
 
-console.log(jsUser)
+// console.log(jsUser)
 
 // If you want to lock the value
 // Object.freeze(jsUser)
@@ -27,12 +27,63 @@ console.log(jsUser)
 
 
 // function
-const myFun = function(){
-    console.log(`hello ${this.name}`)
-}
-console.log(myFun())
+// const myFun = function(){
+//     console.log(`hello ${this.name}`)
+// }
+// console.log(myFun())
 
-const myFun2 = function(){
-    console.log(`hello ${jsUser.age}`)
+// const myFun2 = function(){
+//     console.log(`hello ${jsUser.age}`)
+// }
+// console.log(myFun2())
+
+//singleton object
+
+const tinderUser = new Object();
+// const tinderUser = {}
+
+tinderUser.id = "12345"
+tinderUser.name = "Lio"
+tinderUser.email = "one@gmail.com"
+tinderUser.isLoggedIn = false;
+
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty("isLoggedIn"))
+
+const regularUser ={
+    name : "Aarya",
+    email: "one@gmail.com",
+    fullName: {
+        firstName: "Aarya",
+        lastName: "Patil"
+    },
+    isLoggedIn: false
 }
-console.log(myFun2())
+
+console.log(regularUser.fullName.lastName);
+
+// combination
+const obj1 = {1: "a", 2: "b", 3: "c"}
+const obj2 = {4: "a", 5: "b", 6: "c"}
+
+// const obj3 = {...obj1, ...obj2}
+
+const obj3 = Object.assign({}, obj1, obj2)
+console.log(obj3)
+
+
+// de-structuring
+
+const courseInfo ={
+    name: "Js",
+    courseteacher: "Hitesh",
+    coursePrice: "999"
+}
+
+// console.log(courseInfo.courseteacher) //instead of using this 
+
+const {courseteacher : teacher} = courseInfo;
+console.log(teacher);
