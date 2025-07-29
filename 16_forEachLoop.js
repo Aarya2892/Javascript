d9fdb400e736ea8b6c34f4fe93ad053c1efee922
Aarya Patil <122004182+Aarya2892@users.noreply.github.com>
2025-07-29 11:52:37 +0000

    for_each

---
+++ 16_forEachLoop.js
@@ -0,0 +1,35 @@
+// for_each
+
+const coding = ["java", "c", "c++", "python"]
+
+coding.forEach((item)=>{
+    console.log(item)
+})
+
+// function printMe(item){
+//     console.log(item);
+// }
+// coding.forEach(printMe)
+
+coding.forEach((item, index, arr)=>{
+    console.log(item, index, arr)
+})
+
+const myCoding =[
+    {
+        languageName: "javascript",
+        languageFile: "js"
+    },
+    {
+        languageName: "java",
+        languageFile: "java"
+    },
+    {
+        languageName: "python",
+        languageFile: "py"
+    }
+]
+
+myCoding.forEach((item)=>{
+  console.log(`Language Name : ${item.languageName} and File Name ${item.languageFile}`)
+})
