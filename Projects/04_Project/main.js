c2bdf82b71e3ed127bf314cc999391b9a845864c
Aarya Patil <122004182+Aarya2892@users.noreply.github.com>
2025-08-07 12:04:55 +0000

    Project 4

--- Projects/04_Project/main.js
+++ Projects/04_Project/main.js
@@ -1,6 +1,10 @@
-const form = document.querySelector('form')
 
-const getSlot = document.querySelector('.guesses');
+const random = Math.round(Math.random()*100 +1);
+    // console.log(random);
+
+const submit = document.querySelector('#submit')
+const input = document.querySelector('.input')
+const guessSlot = document.querySelector('.guesses');
 const remaining = document.querySelector('.remaining');
 const lowOrHi = document.querySelector('.lowOrHi')
 const startOver = document.querySelector('.resultParas')
@@ -13,17 +17,47 @@ let numGuess =1
 let playGame = true;
 
 if(playGame){
-    
+  submit.addEventListener('click', (e)=>{
+    e.preventDefault();
+    const guess = parseInt(input.value)
+    validateGuess(guess);
+  })
 }
 
 function validateGuess(guess){
-      
+    if(isNaN(guess)){
+      alert("Please Enter valid number")
+    }else if(guess < 1){
+      alert("Please Enter number more than 1")
+    }else if(guess > 100){
+      alert("Please Enter a number less than 100")
+    }else{
+      prevGuess.push(guess);
+      if(numGuess === 11){
+        displayGuess(guess);
+        displayMsg(`Game Over. Random Number was ${random}`);
+        endGame();
+      }else{
+        displayGuess(guess);
+        checkGuess(guess);
+      }
+    }
 }
 function checkGuess(guess){
-
+    if(guess === random){
+      displayMsg(`You guessed it right`)
+      endGame();
+    }else if(guess < random){
+      displayMsg(`Entered number is Less`)
+    }else if(guess > random){
+      displayMsg(`Entered number is High`)
+    }
 }
 function displayGuess(guess){
-
+    input.value = ''
+    guessSlot.innerHTML += `${guess}`
+    numGuess++;
+    remaining.innerHTML =`${11- numGuess}`
 }
 function displayMsg(msg){
 
@@ -37,27 +71,33 @@ function newGame(){
 }
 
 
-const random = Math.round(Math.random()*100 +1);
-    // console.log(random);
 
-form.addEventListener('submit', (e)=>{
-    e.preventDefault();
 
-    const input = parseInt(document.querySelector('.input').value);
-    const answer = document.querySelector('#answer');
-    const randomNumber = document.querySelector('#randomNumber');
-    const reload = document.querySelector('#reload');
 
-    console.log(input);
-    if(input === random){
-        answer.innerHTML =`<span> Number Match </span>`
-        answer.style.color = "green"
-    }else{
-        answer.innerHTML =`<span> Not same Number </span>`
-        answer.style.color = "red"
-    }
+
+
+// const form = document.querySelector('form')
+// const random = Math.round(Math.random()*100 +1);
+//     // console.log(random);
+
+// form.addEventListener('submit', (e)=>{
+//     e.preventDefault();
+
+//     const input = parseInt(document.querySelector('.input').value);
+//     const answer = document.querySelector('#answer');
+//     const randomNumber = document.querySelector('#randomNumber');
+//     const reload = document.querySelector('#reload');
+
+//     console.log(input);
+//     if(input === random){
+//         answer.innerHTML =`<span> Number Match </span>`
+//         answer.style.color = "green"
+//     }else{
+//         answer.innerHTML =`<span> Not same Number </span>`
+//         answer.style.color = "red"
+//     }
     
-    randomNumber.innerHTML = `<span>${random}</span>`
-    reload.innerHTML =`<a href="/Projects/04_Project/index.html">Reload</a>`
-})
+//     randomNumber.innerHTML = `<span>${random}</span>`
+//     reload.innerHTML =`<a href="/Projects/04_Project/index.html">Reload</a>`
+// })
 
