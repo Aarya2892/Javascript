05c92be54724c77094911608c969fa18c79a0f2a
Aarya Patil <122004182+Aarya2892@users.noreply.github.com>
2025-08-07 11:47:34 +0000

    Project 4

--- Projects/04_Project/main.js
+++ Projects/04_Project/main.js
@@ -1,5 +1,42 @@
 const form = document.querySelector('form')
 
+const getSlot = document.querySelector('.guesses');
+const remaining = document.querySelector('.remaining');
+const lowOrHi = document.querySelector('.lowOrHi')
+const startOver = document.querySelector('.resultParas')
+
+const p = document.createElement('p');
+
+let prevGuess = []
+let numGuess =1
+
+let playGame = true;
+
+if(playGame){
+    
+}
+
+function validateGuess(guess){
+      
+}
+function checkGuess(guess){
+
+}
+function displayGuess(guess){
+
+}
+function displayMsg(msg){
+
+}
+function endGame(){
+    
+}
+
+function newGame(){
+
+}
+
+
 const random = Math.round(Math.random()*100 +1);
     // console.log(random);
 
@@ -9,10 +46,11 @@ form.addEventListener('submit', (e)=>{
     const input = parseInt(document.querySelector('.input').value);
     const answer = document.querySelector('#answer');
     const randomNumber = document.querySelector('#randomNumber');
+    const reload = document.querySelector('#reload');
 
     console.log(input);
     if(input === random){
-        answer.innerHTML =`<span> Same number </span>`
+        answer.innerHTML =`<span> Number Match </span>`
         answer.style.color = "green"
     }else{
         answer.innerHTML =`<span> Not same Number </span>`
@@ -20,6 +58,6 @@ form.addEventListener('submit', (e)=>{
     }
     
     randomNumber.innerHTML = `<span>${random}</span>`
-    
+    reload.innerHTML =`<a href="/Projects/04_Project/index.html">Reload</a>`
 })
 
