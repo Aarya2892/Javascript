9621fb19408b20d2d89d6fc5efa3909105314e25
Aarya Patil <122004182+Aarya2892@users.noreply.github.com>
2025-08-07 07:12:04 +0000

    Project 3

--- Projects/03_Project/main.js
+++ Projects/03_Project/main.js
@@ -1,2 +1,9 @@
 const time = document.querySelector('#time');
 
+// const currentTime = Date.now();
+// time.innerText = `${currentTime}`
+
+setInterval(()=>{
+   let currentTime = new Date();
+   time.innerText = currentTime.toLocaleTimeString();
+},1000)
