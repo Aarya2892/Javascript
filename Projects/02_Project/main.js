5082b94547b783b38d816b2591e87860ea6e07c2
Aarya Patil <122004182+Aarya2892@users.noreply.github.com>
2025-08-07 06:45:06 +0000

    Project 2

--- Projects/02_Project/main.js
+++ Projects/02_Project/main.js
@@ -1,3 +1,4 @@
+
 const form = document.querySelector('form');
 
 form.addEventListener('submit', (e)=>{
@@ -5,15 +6,25 @@ form.addEventListener('submit', (e)=>{
 
      const height= parseInt(document.querySelector('#height').value);
      const Weight = parseInt(document.querySelector('#Weight').value);
-     const results = parseInt(document.querySelector('#result'))
+     const results = document.querySelector('#results');
 
-     if(height === '' || height <0 ){
-        results.innerText = `Please give a valid height ${height}`;
-     }else if(Weight === '' || Weight <0){
-        results.innerText = `Please give a valid weight ${Weight}`;
+     if(height === '' || height < 0 || isNaN(height)){
+        results.innerHTML = `Please give a valid height ${height}`;
+     }else if(Weight === '' || Weight <0 || isNaN(Weight)){
+        results.innerHTML = `Please give a valid weight ${Weight}`;
      }else{
-        const bmi = (Weight / (height * height)/1000).toFixed(2);
-        results.innerText =`${bmi}`
-        console.log(bmi);
+        const bmi = (Weight / ((height * height) / 10000)).toFixed(2);
+        results.innerHTML =`<span>${bmi}<span>`
+      //   console.log(bmi);
+
+      const info = document.querySelector("#info")
+      if(bmi < 18.6){
+         info.innerHTML = `<p>Under Weight</p>`
+      }else if(bmi >=18.6 && bmi <= 24.9){
+         info.innerHTML = `<p>Normal Range</p>`
+      }else if (bmi > 24.9){
+         info.innerHTML = `<p>Overwight</p>`
+      }
      }
+
 });
