de699e113b8aca35d0d5dad03ba5da221a874047
Aarya Patil <122004182+Aarya2892@users.noreply.github.com>
2025-11-04 11:40:08 +0000

    Static

---
+++ 30_Staticprop.js
@@ -0,0 +1,27 @@
+class user{
+    constructor(username){
+        this.username = username
+    }
+    logMe(){
+        console.log(`User Name ${this.username}`);
+    }
+    static createId(){
+        return `123`
+    }
+}
+
+const Nike = new user("Piku")
+// console.log(Nike.createId());
+
+class Teacher extends user{
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const Puma = new Teacher("Puma", "one@gmail.com")
+Puma.logMe();
+// console.log(Puma.createId());
+
+
